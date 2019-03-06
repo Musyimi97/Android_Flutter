6f321ff060d864da1106b805cdd3c4c3a51552bd
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-06 21:05:20 +0300

    LGTM

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body:  Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpeg'),
-          Text('Food Paradise')
-        ],),),
-      ),
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container( 
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),),
+             
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpeg'),
+                    Text('Food Paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
